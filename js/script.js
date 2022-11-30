5f6c5a8769aae453394153f760b827b2dccd4734
Jan Kędzierski <jkedzierski087@gmail.com>
2022-11-30 18:26:21 +0100

    taks number 1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = 'kamień';
+
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'nożyce';
+
+printMessage('Zagrałem ' + 'nożyce' + '!Jeśli Twój ruch to kamień, to wygrywasz!');
